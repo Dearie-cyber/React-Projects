b605b92a260648c60ad1db890e552c88df54c655
Dearie <eburujacintha@gmail.com>
2023-07-25 22:41:53 +0100

    lifted state

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -7,11 +7,18 @@ const initialItems = [
 ];
 
 export default function App(){
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item){
+    setItems(items=> [...items, item]);
+  }
+
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems}/>
+      <PackingList items={items}/>
       <Stats />
     </div>
     )
@@ -21,17 +28,20 @@ function Logo(){
   return <h1>🌴Far Away💼</h1>
 }
 
-function Form(){
+function Form({onAddItems}){
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+  
 
+  
   function handleSubmmit(e){
     e.preventDefault();
 
     if (!description) return;
-
+ 
     const newItem = {description, quantity, packed: false, id: Date.now()}
  
+    onAddItems(newItem);
     setDescription('');
     setQuantity(1);
   }
@@ -53,11 +63,11 @@ function Form(){
   )
 }
 
-function PackingList(){
+function PackingList({ items }){
   return(
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
           ))}
       </ul>
